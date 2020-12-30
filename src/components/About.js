138aaf859653c07bf7d1012a69e3046cd7e3d049
murselaysan <64005116+murselaysan@users.noreply.github.com>
2020-12-30 20:01:57 +0000

    Update About Component

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import React from 'react';
 const About = () => (
   <div>
     <h2>About</h2>
-    This example shows how to use React Router!
+    This example shows how to use React Router woww!
   </div>
 );
 
